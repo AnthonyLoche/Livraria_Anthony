972cfdbf0718150154295ddf6437ffb96c3d9154
AnthonyLoche <anthonylocheifc@gmail.com>
2025-05-28 15:35:56 -0300

    FEAT: criação da API para Categoria

--- app/settings.py
+++ app/settings.py
@@ -131,8 +131,6 @@ else:
         },
     }
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SPECTACULAR_SETTINGS = {
--- app/urls.py
+++ app/urls.py
@@ -7,11 +7,16 @@ from drf_spectacular.views import (
 )
 from rest_framework.routers import DefaultRouter
 
+
 from core.views import UserViewSet
+from core.views import CategoriaViewSet # nova linha
+
 
 router = DefaultRouter()
 
 router.register(r'usuarios', UserViewSet, basename='usuarios')
+router.register(r"categorias", CategoriaViewSet) # nova linha
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
